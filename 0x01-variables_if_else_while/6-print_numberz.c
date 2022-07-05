28ab0de9ee4df0e9d1d9c98431ca86fb905d7c50
John_Isah <ijohnabdulmalik@gmail.com>
2022-07-04 18:43:57 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main -> Entru point
+* Return: always return 0
+*/
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		putchar('0' + a);
+		putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main -> Entru point
+* Return: always return 0
+*/
+int main(void)
+{
+	unsigned char a = '0';
+	int i;
+
+	for (i = 0; i < 6; i++)
+		putchat(a);
+		a++;
+	for 
+}
