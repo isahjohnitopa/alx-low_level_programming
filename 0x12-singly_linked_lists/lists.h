416ade2a6f1b4491c7e4baa760cde9762a42e7b2
John_Isah <ijohnabdulmalik@gmail.com>
2022-08-12 18:41:40 -0700

    header file fixed

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,12 +8,12 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: lingth of the string
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
-typedef strut list_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
