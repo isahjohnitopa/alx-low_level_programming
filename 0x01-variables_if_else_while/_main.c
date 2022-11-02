a9bcce691d275ac70232e8884de89c9a97a82e25
isahjohna <isahjohna@gmail.com>
2022-11-02 19:47:53 +0100

    Just learning

---
+++ 0x01-variables_if_else_while/_main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - solving the assignment
+ * description - A program for school grading system
+ * 80 - 100 == A
+ * 70 - 79  == B
+ * 65 - 69  == C
+ * 60 - 64  == D
+ * 50 - 59  == E
+ * Below 50 == F
+ * Return 0
+*/
+
+int main(void)
+{
+
+    return (0);
+}
