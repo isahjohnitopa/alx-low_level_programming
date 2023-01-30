c4cae78656aff4f386c2dc5081f07e096c40a504
isahjohna <isahjohna@gmail.com>
2023-01-29 17:25:07 -0800

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
+
