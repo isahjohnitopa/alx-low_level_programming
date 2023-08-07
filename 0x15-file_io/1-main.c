355c9f4f3c78df469a990a8b6a9706d00fde46d6
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-07 19:53:13 +0100

    function that create a file

--- 0x15-file_io/1-main.c
+++ 0x15-file_io/1-main.c
@@ -9,15 +9,15 @@
 */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
 
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
 }
