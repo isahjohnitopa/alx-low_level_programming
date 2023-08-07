549c3f1d29c98742e083a593b2d68c169ca26d92
isahjohna <ijohnabdulmalik@gmail.com>
2023-08-07 23:31:34 +0100

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,87 +1,84 @@
 #include "main.h"
 
 /**
- * main - copies the content of a file to another file.
- * @ac: The argument count
+ * close_handler - function to handle close eerors
+ * @src: source file
+ * @dest: destination file
+ * Return: nothing
+ */
+void close_handler(int src, int dest)
+{
+	int code;
+
+	code = close(src);
+	if (code == FILE_ERROR)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", src);
+		exit(CANNOT_CLOSE);
+	}
+	code = close(dest);
+	if (code == FILE_ERROR)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", dest);
+		exit(CANNOT_CLOSE);
+	}
+}
+
+/**
+ * error_handler - function to handle the file errors
+ * @dest: destination file
+ * @src: source file
+ * @av: argument values
+ *
+ * Return: nothing
+ */
+void error_handler(int src, int dest, char **av)
+{
+	if (src == FILE_ERROR)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(DOES_NOT_EXIST_OR_UNABLE_TO_READ);
+	}
+	if (dest == FILE_ERROR)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", av[2]);
+		exit(CANNOT_CREATE_OR_WRITE);
+	}
+}
+
+/**
+ * main - copies the content of a file to anoter file
+ * @ac: the argument count
  * @av: the argument vector
  *
- * Return: Return 0 on success
+ * Return: Return 0 on success or anything else on error
  */
 int main(int ac, char **av)
 {
-	int from, to, r, w;
-	char *buff;
+	int src, dest;
+	char buffer[BUFFER_SIZE];
+	ssize_t r, w;
 
-	if (ac != 3)
+	if (ac != ARGUMENT_COUNT_EXPECTED)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		exit(INVALID_NUMBER_OF_ARGUMENTS);
+	}
+	src = open(av[1], O_RDONLY);
+	dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_handler(src, dest, av);
+	r =  BUFFER_SIZE;
+	
+	while (r == BUFFER_SIZE)
+	{
+		r = read(src, buffer, BUFFER_SIZE);
+		if (r == FILE_ERROR)
+			error_handler(FILE_ERROR, 0, av);
+		w = write(dest, buffer, r);
+		if (w == FILE_ERROR)
+			error_handler(0, FILE_ERROR, av);
 	}
 
-	buff = create_buffer(av[2]);
-	from = open(av[1], O_RDONLY);
-	r = read(from, buff, 1024);
-	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (from == -1 || r == -1)
-		{
-			dprintf(2, "Error: Can't read from  %s\n", av[1]);
-			free(buff);
-			exit(98);
-		}
-
-		w = write(to, buff, r);
-		if (to == -1 || w == -1)
-		{
-			dprintf(2, "Error: Can't write to  %s\n", av[2]);
-			free(buff);
-			exit(99);
-		}
-
-		r = read(from, buff, 1024);
-		to = open(av[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-
-	free(buff);
-	close_file(from);
-	close_file(to);
-
+	close_handler(src, dest);
 	return (0);
-}
-
-/**
- * create_buffer - allocate 1024 bytes for a buffer
- * @file: the name of the file buffeer is storing chars for
- *
- * Return: Return pointer tot he newly-allocated buffer
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (!buffer)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file);
-		exit(99);
-	}
-	return (buffer);
-}
-
-/**
- * close_file - close the file descriptors
- * @fd: teh file descriptor to be closed
- */
-void close_file(int fd)
-{
-	int c;
-
-	c = close(fd);
-
-	if (c == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
+#define FILE_ERROR -1
+#define INVALID_NUMBER_OF_ARGUMENTS 97
+#define DOES_NOT_EXIST_OR_UNABLE_TO_READ 98
+#define CANNOT_CREATE_OR_WRITE 99
+#define CANNOT_CLOSE 100
+#define ARGUMENT_COUNT_EXPECTED 3
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
